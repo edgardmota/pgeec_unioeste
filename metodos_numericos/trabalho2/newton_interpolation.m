6d7b6a1c00b996cfec92df466bc59ba5e36fc0f9
Edgard Mota <edgardmota@gmail.com>
2017-06-21 20:17:13 -0300

    Métodos Numéricos: Trabalho 2 - Newton com cálculo de erro

--- metodos_numericos/trabalho2/newton_interpolation.m
+++ metodos_numericos/trabalho2/newton_interpolation.m
@@ -1,15 +1,9 @@
-function pn = newton_interpolation(X,Y,n,points)
+function pn = newton_interpolation(X,Y,d,n,points)
   valid_args = false;
   if valid_inputs(X,Y)
-    if (~(exist('n','var')) || (size(n,1) == 0) || (size(points,1) == 0))
-      n = get_n_size(X);
-      points = [1:n+1];
-      valid_args = true;
-    elseif exist('points','var')
       if (check_points(X,n,points))
         valid_args = true;
       end
-    end
   end
   if valid_args
     n_X = zeros(1,n+1);
@@ -18,10 +12,9 @@ function pn = newton_interpolation(X,Y,n,points)
       n_X(i) = X(points(i));
       n_Y(i) = Y(points(i));
     end
-    d = divided_differences(n_X, n_Y);
-    pn_str = strcat('@(x)',num2str(d(1,1)));
+    pn_str = strcat('@(x)',num2str(d(points(1),1)));
     for i = 2:(n+1)
-      x_xj = num2str(d(1,i));
+      x_xj = num2str(d(points(1),i));
       for j = 1:(i - 1)
         x_xj = strcat(x_xj,'*(x-(',num2str(n_X(j)),'))');
       end
@@ -31,6 +24,26 @@ function pn = newton_interpolation(X,Y,n,points)
   else
     error('Erro: argumentos invalidos!');
   end
+end
+
+function m = max_dd_n_p_1(d,n)
+  pos = n + 2;
+  if (pos > size(d,1)) 
+    pos = pos - 1
+  end
+  l = reshape(d(:,pos),1,size(d,1));
+  for i = 1:n+1
+    l(i) = abs(l(i));
+  end
+  m = max(l);
+end
+
+function enx = estimated_error(x,X,d,n,points)
+  enx = max_dd_n_p_1(d,n);
+  for i = 1:n+1
+    enx = enx*(x-X(points(i)));
+  end
+  enx = abs(enx);
 end
 
 function d = divided_differences(X, Y)
@@ -66,7 +79,7 @@ function valid = check_points(X,n,points)
   valid = (max_index <= X_size) & (min_index >= 1) & (points_size == sum(count)) & (points_size == (n + 1));
 end
 
-function n = get_n_size(X)
+function n = get_size(X)
   n = size(X,2) - 1;
 end
 
@@ -82,18 +95,20 @@ end
 function [n, points] = get_n_points
   n = input('Grau do polinomio aproximador (n) (vazio para grau maximo): ');
   if (size(n,1) ~= 0)
-    points = input('Vetor com os indices de X a serem usados: ');
+    point = input(sprintf('Indice de X a partir do qual usar %d pontos: ',n + 1));
+    points = [point:point+n];
   else
+    n = 0;
     points = [];
   end
 end
 
-function test_x(pn)
+function test_x(pn,X,d,n,points)
   disp(strcat("\npn(x)=",func2str(pn)));
   x = input("\nDigite um valor para x (vazio para sair): ",'s');
   while (~strcmp(x,''))
     x = eval(x);
-    disp(strcat("\nPn(",num2str(x),')=',num2str(pn(x))));
+    disp(strcat("\nPn(",num2str(x),')=',num2str(pn(x)),' (Erro aproximado:  ',num2str(estimated_error(x,X,d,n,points)),')'));
     disp(strcat("\npn(x)=",func2str(pn)));
     x = input("\nDigite um valor para x (vazio para sair): ",'s');
   end
@@ -101,6 +116,10 @@ end
 
 [X, Y] = get_XY;
 [n, points] = get_n_points;
-pn=newton_interpolation(X,Y,n,points);
-test_x(pn);
-
+if (n == 0)
+  n = get_size(X);
+  points = [1:n+1];
+end
+d = divided_differences(X,Y);
+pn = newton_interpolation(X,Y,d,n,points);
+test_x(pn,X,d,n,points);
